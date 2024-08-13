d34d29449d7029b5f3c59e1148d025e489f26687
AdalmoDevelopment <jthomas@adalmo.com>
2024-08-13 10:07:32 +0200

    Escubi

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 'use client'
 import React from "react";
 import ParticlesBackground from "./ParticlesBackground";
+import Image from 'next/image'
 
 const page = () => {
     return (
@@ -8,8 +9,23 @@ const page = () => {
             <ParticlesBackground />
             <div className='w-full h-full flex items-center justify-center md:px-10 px-5'>
                 {/* Content for the landing page */}
-                <p className="animate-fade-right animate-delay-300 animate-once text-center">Escubi</p>
-            </div>
+                {/* <img src={require('../public/react_logo-512.webp')} />  */}
+                <Image
+                    src="/logo.png"
+                    width={300}
+                    height={300}
+                    alt="Picture of the author"
+                    className="animate-fade-right animate-delay-900 animate-once "
+                />
+                                <Image
+                    src="/nombre.png"
+                    width={500}
+                    height={500}
+                    alt="Picture of the author"
+                    className="animate-fade-up animate-delay-500 animate-once "
+                />
+
+                </div>
         </div>
     );
 };
