29bd5d3d8a85d9f8002570d0f42b98f715d76822
AdalmoDevelopment <jthomas@adalmo.com>
2024-08-13 10:46:06 +0200

    Escubi

--- app/page.jsx
+++ app/page.jsx
@@ -11,18 +11,16 @@ const page = () => {
                 {/* Content for the landing page */}
                 {/* <img src={require('../public/react_logo-512.webp')} />  */}
                 <Image
-                    src="/logo.png"
+                    src="/Isotipo BT.svg"
                     width={300}
                     height={300}
-                    alt="Picture of the author"
                     className="animate-fade-right animate-delay-900 animate-once "
                 />
-                                <Image
-                    src="/nombre.png"
-                    width={500}
-                    height={500}
-                    alt="Picture of the author"
-                    className="animate-fade-up animate-delay-500 animate-once "
+                <Image
+                    src="/Logotipo BT.svg"
+                    width={600}
+                    height={600}
+                    className="animate-fade-up animate-delay-500 animate-once ml-10"
                 />
 
                 </div>
